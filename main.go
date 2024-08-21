9c375c25b4daf615de065eb7f0b12ede135199d3
Ricardo Arat Ibarra Campa <5161960+RickArt@users.noreply.github.com>
2024-08-21 02:18:54 -0600

    Added add, toggle and exit options

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
 	"strconv"
+	"strings"
 )
 
 const (
-	PRINT_OPTION     = 1
-	ADD_OPTION       = 2
-	MARK_DONE_OPTION = 3
-	REMOVE_OPTION    = 4
-	EXIT_OPTION      = 5
-	MAX_OPTION       = 5
+	PRINT_OPTION            = 1
+	ADD_OPTION              = 2
+	TOGGLE_DONE_OPTION      = 3
+	REMOVE_OPTION           = 4
+	SAVE_AND_EXIT_OPTION    = 5
+	DISCARD_AND_EXIT_OPTION = 6
+	MAX_OPTION              = 6
 )
 
 var fileName string = "list.csv"
@@ -22,9 +26,10 @@ var optionsMessage string = `
 Select an option:
   1.- Print list
   2.- Add task
-  3.- Mark as DONE
+  3.- Toggle DONE
   4.- Remove task
-  5.- Exit
+  5.- Save and Exit
+  6.- Discard changes and Exit
 `
 
 func main() {
@@ -36,7 +41,7 @@ func main() {
 
 	reader := csv.NewReader(file)
 
-	taskMap := loadTaskMap(reader)
+	taskMap, highestId := loadTaskMap(reader)
 
 	for {
 		fmt.Println(optionsMessage)
@@ -47,14 +52,21 @@ func main() {
 		switch userOption {
 		case PRINT_OPTION:
 			printTasks(taskMap)
+		case ADD_OPTION:
+			addTask(taskMap, &highestId)
+		case TOGGLE_DONE_OPTION:
+			markTaskAsDone(taskMap)
 		case REMOVE_OPTION:
 			taskIdToDelete, err := readTaskIdToDelete()
 			if err != nil {
 				log.Fatal(err)
 			}
 			delete(taskMap, taskIdToDelete)
-		case EXIT_OPTION:
-			return
+		case SAVE_AND_EXIT_OPTION:
+			saveList(taskMap, file)
+			fallthrough
+		case DISCARD_AND_EXIT_OPTION:
+			os.Exit(0)
 		}
 	}
 }
@@ -86,7 +98,7 @@ func printTasks(taskMap map[int]Task) {
 	}
 }
 
-func loadTaskMap(reader *csv.Reader) (taskMap map[int]Task) {
+func loadTaskMap(reader *csv.Reader) (taskMap map[int]Task, highestId int) {
 	taskMap = make(map[int]Task)
 	records, err := reader.ReadAll()
 	if err != nil {
@@ -102,12 +114,15 @@ func loadTaskMap(reader *csv.Reader) (taskMap map[int]Task) {
 			Done:        done,
 		}
 		taskMap[id] = data
+		if id > highestId {
+			highestId = id
+		}
 	}
-	return taskMap
+	return taskMap, highestId
 }
 
 func readTaskIdToDelete() (int, error) {
-	fmt.Println("Select the id number to delete the task")
+	fmt.Println("Select the id number to delete the task: ")
 	var input string
 	_, err := fmt.Scanln(&input)
 
@@ -122,4 +137,60 @@ func readTaskIdToDelete() (int, error) {
 	}
 
 	return parsedInput, nil
+}
+
+func markTaskAsDone(taskMap map[int]Task) {
+	fmt.Println("Select the id number to toggle Done: ")
+	var input string
+	_, err := fmt.Scanln(&input)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	parsedInput, err := strconv.Atoi(input)
+	if err != nil {
+		log.Println("Invalid option")
+	}
+
+	task := taskMap[parsedInput]
+	task.Done = !task.Done
+	taskMap[parsedInput] = task
+}
+
+func saveList(taskMap map[int]Task, file *os.File) {
+	writer := csv.NewWriter(file)
+	file.Truncate(0)
+	file.Seek(0, 0)
+
+	for _, task := range taskMap {
+		writer.Write(taskToRecord(task))
+	}
+	writer.Flush()
+}
+
+func taskToRecord(task Task) (record []string) {
+	return []string{
+		strconv.Itoa(task.Id),
+		task.Description,
+		strconv.FormatBool(task.Done),
+	}
+}
+
+func addTask(taskMap map[int]Task, highestId *int) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Task Description: ")
+	taskDescription, err := reader.ReadString('\n')
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	taskDescription = strings.TrimSpace(taskDescription)
+	*highestId = *highestId + 1
+	task := Task{
+		Id:          *highestId,
+		Description: taskDescription,
+		Done:        false,
+	}
+	taskMap[*highestId] = task
 }
